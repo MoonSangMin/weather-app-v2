5648aa4aea6a4ee9c6fe0a9e011a651b23efc389
Moon <msm2230@naver.com>
2022-05-29 14:54:27 +0900

    Weather App with learning Functional Components

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
+import * as Location from 'expo-location';
+import API_KEY from './keys';
+const windowWidth = Dimensions.get('window').width;
 
 export default function App() {
+  const [city, setCity] = useState("Seoul");
+  const [days, setDays] = useState([]);
+  const [permission, setPermission] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setPermission(false);
+      }
+
+      const { coords: {latitude, longitude} } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+			const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
+			setCity(location[0].city);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY.API_KEY}&units=metric`);
+      const json = await response.json();
+      setDays(json.list);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView pagingEnabled horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
+        {days.length === 0 ? (
+        <View style={styles.day}>
+          <ActivityIndicator color="white" size="large"/>
+        </View>
+        ) : (
+          days.map((day, index) =>
+          <View key={index} style={styles.day}>
+            <Text style={styles.temp}>{day.main.temp}</Text>
+            <Text style={styles.description}>{day.weather[0].description}</Text>
+          </View>)
+          )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +52,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "coral"
   },
-});
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 45,
+  },
+  weather: {
+    
+  },
+  day: {
+    width: windowWidth,
+    alignItems: "center"
+  },
+  temp: {
+    fontSize: 80,
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 50
+  }
+})
